867652dfc575b2f4a3ccbb9293ab54eca07c9f0f
david8zhang <david8zhang@berkeley.edu>
2024-12-30 13:27:09 -0800

    spawn player in center of the screen

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,7 +1,8 @@
 class_name Game
 extends Node2D
 
-@onready var player = $Player
+@onready var player = $Player as Player
 
 func _ready():
-	pass
+	var viewport_rect = get_viewport_rect()
+	player.global_position = viewport_rect.get_center()
